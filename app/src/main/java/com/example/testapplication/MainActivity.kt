23d84bb72c0541dc918a45b0525027fb1f41be91
LimGwangSu <didth0812@gmail.com>
2022-12-21 22:43:38 +0900

    tttttt

--- app/src/main/java/com/example/testapplication/MainActivity.kt
+++ app/src/main/java/com/example/testapplication/MainActivity.kt
@@ -10,5 +10,6 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
         Log.d("test","test")
         Log.d("limgs1", "limgs1")
+        Log.d("test~~~", "test@@@@")
     }
 }
