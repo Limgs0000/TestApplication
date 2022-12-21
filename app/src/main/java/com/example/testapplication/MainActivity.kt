e28f0bd36a32dc579b41ded57c0d7d4b753ee098
oyeongjun <icy9409@daum.net>
2022-12-21 22:46:40 +0900

    버튼 가져옴

--- app/src/main/java/com/example/testapplication/MainActivity.kt
+++ app/src/main/java/com/example/testapplication/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.testapplication
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -12,5 +13,9 @@ class MainActivity : AppCompatActivity() {
         Log.d("test","test")
         Log.d("test","test")
         Log.d("test","test")
+
+
+
+        findViewById<Button>(R.id.btn_test)
     }
 }
